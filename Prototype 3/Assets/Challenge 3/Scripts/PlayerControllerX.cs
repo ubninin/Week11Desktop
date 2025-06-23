7159ce3f077a5e481f8b5bec0176078ccd30543c
ubninin <144878603+ubninin@users.noreply.github.com>
2025-06-23 21:16:44 +0900

    3장 과제 완료

--- Prototype 3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Prototype 3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -8,6 +8,8 @@ public class PlayerControllerX : MonoBehaviour
 
     public float floatForce;
     private float gravityModifier = 1.5f;
+    public float bounceForce = 10.0f; // 바닥에서 튕겨지는 힘
+
     private Rigidbody playerRb;
 
     public ParticleSystem explosionParticle;
@@ -16,24 +18,38 @@ public class PlayerControllerX : MonoBehaviour
     private AudioSource playerAudio;
     public AudioClip moneySound;
     public AudioClip explodeSound;
+    public AudioClip boingSound;
 
+    // 추가: 플레이어가 충분히 낮은지 확인하는 변수
+    private bool isLowEnough = true;
+    public float upperLimit = 9.0f; // 플레이어의 최대 높이 제한
 
-    // Start is called before the first frame update
     void Start()
     {
+        playerRb = GetComponent<Rigidbody>(); // 초기화
         Physics.gravity *= gravityModifier;
         playerAudio = GetComponent<AudioSource>();
 
-        // Apply a small upward force at the start of the game
         playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
-
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        isLowEnough = transform.position.y < upperLimit;
+
+        // 스페이스바가 눌리고 게임이 끝나지 않았으며, 플레이어가 충분히 낮은 경우에만 위로 힘을 더함
+        if (Input.GetKey(KeyCode.Space) && !gameOver && isLowEnough)
+        {
+            playerRb.AddForce(Vector3.up * floatForce);
+        }
+        else if (transform.position.y >= upperLimit)
+        {
+            // 플레이어가 upperLimit에 도달했을 때, 더 이상 힘을 추가하지 않음
+            playerRb.velocity = new Vector3(playerRb.velocity.x, 0, playerRb.velocity.z);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && !gameOver && isLowEnough)
         {
             playerRb.AddForce(Vector3.up * floatForce);
         }
@@ -41,7 +57,7 @@ public class PlayerControllerX : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        // if player collides with bomb, explode and set gameOver to true
+
         if (other.gameObject.CompareTag("Bomb"))
         {
             explosionParticle.Play();
@@ -49,17 +65,20 @@ public class PlayerControllerX : MonoBehaviour
             gameOver = true;
             Debug.Log("Game Over!");
             Destroy(other.gameObject);
-        } 
+        }
 
-        // if player collides with money, fireworks
+
         else if (other.gameObject.CompareTag("Money"))
         {
             fireworksParticle.Play();
             playerAudio.PlayOneShot(moneySound, 1.0f);
             Destroy(other.gameObject);
-
+        }
+        else if (other.gameObject.CompareTag("Ground"))
+        {
+            playerAudio.PlayOneShot(boingSound, 1.0f);
+            playerRb.AddForce(Vector3.up * bounceForce, ForceMode.Impulse); // 바닥에서 튀어 오름
         }
 
     }
-
 }
